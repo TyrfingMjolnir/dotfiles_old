464305fd388d8b72b7b131dcb0790b158d99652c
Tyrfing Mjolner <4lphamal3@gmail.com>
2016-09-18 14:00:16 +0200

    Update archMinimumDevTools.sh

--- archMinimumDevTools.sh
+++ archMinimumDevTools.sh
@@ -1,2 +1,4 @@
 sudo pacman -S tmux vim curl rxvt-unicode mpv youtube-dl
-curl -kLO 
+curl -kLO https://raw.githubusercontent.com/TyrfingMjolnir/dotfiles/master/.tmux.conf
+curl -kLO https://raw.githubusercontent.com/TyrfingMjolnir/dotfiles/master/.Xresources
+curl -kLO https://raw.githubusercontent.com/TyrfingMjolnir/dotfiles/master/.vimrc
