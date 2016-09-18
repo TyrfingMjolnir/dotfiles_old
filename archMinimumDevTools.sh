c3d3261e260914d0e57f3ffbc62d2ff574268023
Tyrfing Mjolner <4lphamal3@gmail.com>
2016-09-18 15:39:26 +0200

    Update archMinimumDevTools.sh

--- archMinimumDevTools.sh
+++ archMinimumDevTools.sh
@@ -1,4 +1,5 @@
 sudo pacman -S exa tmux tidy vim curl rxvt-unicode mpv youtube-dl couchdb redis postgresql orientdb
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
 curl -kLO https://raw.githubusercontent.com/TyrfingMjolnir/dotfiles/master/.tmux.conf
 curl -kLO https://raw.githubusercontent.com/TyrfingMjolnir/dotfiles/master/.Xresources
 curl -kLO https://raw.githubusercontent.com/TyrfingMjolnir/dotfiles/master/.vimrc
