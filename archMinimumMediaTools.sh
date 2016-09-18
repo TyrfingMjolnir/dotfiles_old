9958f9b2650057f0fc8086bb362654e37f7419cd
Tyrfing Mjolner <4lphamal3@gmail.com>
2016-09-18 14:00:45 +0200

    Update archMinimumMediaTools.sh

--- archMinimumMediaTools.sh
+++ archMinimumMediaTools.sh
@@ -1,2 +1,4 @@
 sudo pacman -S tmux vim curl rxvt-unicode mpv youtube-dl handbrake simplescreenrecorder ffmpeg mencoder
-curl -kLO 
+curl -kLO https://raw.githubusercontent.com/TyrfingMjolnir/dotfiles/master/.tmux.conf
+curl -kLO https://raw.githubusercontent.com/TyrfingMjolnir/dotfiles/master/.Xresources
+curl -kLO https://raw.githubusercontent.com/TyrfingMjolnir/dotfiles/master/.vimrc
