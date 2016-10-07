8483b27892f3f4a015fc5e018f877451e42cca33
Tyrfing Mjolner <4lphamal3@gmail.com>
2016-10-07 09:02:37 +0200

    Create macOSX.sh

---
+++ macOSX.sh
@@ -0,0 +1,5 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew cask install exa
+brew install vim tmux nvm
+nvm install nodejs
+npm i -g json nodemon
