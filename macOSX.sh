f4403f5fc6897b14c7907af3a5845ed5d6f8726a
Tyrfing Mjolner <4lphamal3@gmail.com>
2017-05-04 09:35:39 +0200

    Update macOSX.sh

--- macOSX.sh
+++ macOSX.sh
@@ -5,3 +5,10 @@ nvm install nodejs
 npm i -g json nodemon
 
 echo 'alias tb="nc termbin.com 9999"' >> ~/.bash_profile
+
+echo 'alias ll="exa -l"'    >> ~/.bash_profile
+echo 'alias l1="exa -1"'    >> ~/.bash_profile
+echo 'alias llt="exa -T"'   >> ~/.bash_profile
+echo 'alias lllt="exa -lT"' >> ~/.bash_profile
+echo 'alias lla="exa -la"'  >> ~/.bash_profile
+echo 'alias llh="exa -lh"'  >> ~/.bash_profile
