3602c24c7bed7170382affd64aabf7f611642e5d
Tyrfing Mjolner <4lphamal3@gmail.com>
2017-11-07 17:13:30 +0100

    Update macOSX.sh

--- macOSX.sh
+++ macOSX.sh
@@ -1,10 +1,10 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew cask install exa
+brew install exa
 brew install vim ctags tmux nvm
-nvm install nodejs
+nvm install --lts
 npm i -g json nodemon
 
-echo 'alias tb="nc termbin.com 9999"' >> ~/.bash_profile
+echo 'alias tb="(exec 3<>/dev/tcp/termbin.com/9999; cat >&3; cat <&3; exec 3<&-)"' >> ~/.bash_profile
 
 echo 'alias ll="exa -l"'    >> ~/.bash_profile
 echo 'alias l1="exa -1"'    >> ~/.bash_profile
