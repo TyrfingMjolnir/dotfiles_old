74ab039c2e19d75b7c38f3363315580047d68278
Tyrfing Mjolner <4lphamal3@gmail.com>
2017-11-19 01:13:26 +0100

    Update archMinimumDevTools.sh

--- archMinimumDevTools.sh
+++ archMinimumDevTools.sh
@@ -1,4 +1,4 @@
-sudo pacman -S exa tmux tidy vim ctags curl rxvt-unicode mpv youtube-dl couchdb redis postgresql orientdb
+sudo pacman -S exa tmux tidy vim ctags curl rxvt-unicode mpv youtube-dl couchdb redis postgresql orientdb ttf-inconsolata
 yaourt -S nvm firefox-aurora qira
 curl -kLO https://raw.githubusercontent.com/TyrfingMjolnir/dotfiles/master/.tmux.conf
 curl -kLO https://raw.githubusercontent.com/TyrfingMjolnir/dotfiles/master/.Xresources
