0992e93ca7c9237c7137cf335fdc7d0888d831e2
Tyrfing Mjolner <4lphamal3@gmail.com>
2018-05-19 11:21:47 +0200

    Update archMinimumDevTools.sh

--- archMinimumDevTools.sh
+++ archMinimumDevTools.sh
@@ -1,5 +1,13 @@
 sudo pacman -S exa tmux tidy vim ctags curl rxvt-unicode mpv youtube-dl couchdb redis postgresql orientdb ttf-inconsolata scim
 yaourt -S nvm firefox-aurora qira
+nvm i --lts
+nvm install --lts
+nvm use --lts 
+nvm alias default node
+sudo npm i -g json nodemon
+
+
+
 curl -kLO https://raw.githubusercontent.com/TyrfingMjolnir/dotfiles/master/.tmux.conf
 curl -kLO https://raw.githubusercontent.com/TyrfingMjolnir/dotfiles/master/.Xresources
 curl -kLO https://raw.githubusercontent.com/TyrfingMjolnir/dotfiles/master/.vimrc
