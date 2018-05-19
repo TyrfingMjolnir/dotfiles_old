4d6fc68d54d0ef7b59cabfff982eb94912ac6df3
Tyrfing Mjolner <4lphamal3@gmail.com>
2018-05-19 11:27:22 +0200

    Update archMinimumDevTools.sh

--- archMinimumDevTools.sh
+++ archMinimumDevTools.sh
@@ -1,7 +1,7 @@
 sudo pacman -S adobe-source-code-pro-fonts ttf-inconsolata
 sudo pacman -S exa tmux tidy vim ctags curl rxvt-unicode mpv youtube-dl scim
 sudo pacman -S couchdb redis postgresql orientdb
-sudo pacman -S  firefox-developer-edition
+sudo pacman -S firefox-developer-edition chromium
 yaourt -S qira
 curl -kLO https://raw.githubusercontent.com/TyrfingMjolnir/dotfiles/master/.tmux.conf
 curl -kLO https://raw.githubusercontent.com/TyrfingMjolnir/dotfiles/master/.Xresources
