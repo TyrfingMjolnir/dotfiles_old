cb7f1cce0cbc01d158c081d7b51d2b24feb55e99
Tyrfing Mjølner <4lphamal3@gmail.com>
2019-11-13 11:23:50 +0100

    Update archMinimumDevTools.sh

--- archMinimumDevTools.sh
+++ archMinimumDevTools.sh
@@ -4,7 +4,12 @@ sudo pacman -S couchdb redis postgresql ack the_silver_searcher
 sudo pacman -S firefox-developer-edition chromium
 sudo pacman -S arch-install-scripts
 
-yaourt -S qira
+## From AUR
+https://aur.archlinux.org/packages/qira/
+https://aur.archlinux.org/packages/swift-bin/
+
+## From GIT
+https://github.com/wg/wrk
 
 curl -kLO https://raw.githubusercontent.com/TyrfingMjolnir/dotfiles/master/.tmux.conf
 curl -kLO https://raw.githubusercontent.com/TyrfingMjolnir/dotfiles/master/.Xresources
