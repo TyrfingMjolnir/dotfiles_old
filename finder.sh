1bc2c30dc72507750b822fed9a417a19c7540986
Tyrfing Mjølner <4lphamal3@gmail.com>
2023-05-13 11:42:43 +0200

    Update finder.sh
    :%s/sudo/_sudo_/g

--- finder.sh
+++ finder.sh
@@ -27,7 +27,7 @@ defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
 # Set the Defaults screenshot format; avialable options: PNG TIFF PDF JPG
 defaults write com.apple.screencapture type PNG
 
-# Set Finder's LoginWindowText; this setting in particular requires sudo to run
+# Set Finder's LoginWindowText; this setting in particular requires _sudo_ to run
 defaults write /Library/Preferences/com.apple.loginwindow LoginwindowText "If you find me astray from my owner please contact me on phone text or FaceTime audio at: +47 1234 5678"
 
 # Enable snap-to-grid for icons on the desktop and in other icon views
