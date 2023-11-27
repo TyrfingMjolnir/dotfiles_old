3aba9445c042f1b1e5ea7be3b52fd7c4d46d77f2
Tyrfing Mjølner <4lphamal3@gmail.com>
2023-11-27 14:54:20 +0100

    Update archMinimumDevTools.sh
    Removed scim added rust

--- archMinimumDevTools.sh
+++ archMinimumDevTools.sh
@@ -1,17 +1,24 @@
 sudo pacman -S adobe-source-code-pro-fonts ttf-inconsolata
-sudo pacman -S exa tmux tidy vim ctags curl st termite terminator rxvt-unicode mpv youtube-dl scim
+sudo pacman -S eza tmux tidy neovim curl mpv ytd-dl
 sudo pacman -S couchdb redis postgresql ack the_silver_searcher
 sudo pacman -S firefox-developer-edition chromium
-sudo pacman -S arch-install-scripts
+sudo pacman -S arch-install-scripts rust
 
 ## From AUR
-https://aur.archlinux.org/packages/qira/
-https://aur.archlinux.org/packages/swift-bin/
+waterfox
 
 ## From GIT
-https://github.com/wg/wrk
-
+cd ~
 curl -kLO https://raw.githubusercontent.com/TyrfingMjolnir/dotfiles/master/.tmux.conf
 curl -kLO https://raw.githubusercontent.com/TyrfingMjolnir/dotfiles/master/.Xresources
 curl -kLO https://raw.githubusercontent.com/TyrfingMjolnir/dotfiles/master/.vimrc
 curl -kLO https://raw.githubusercontent.com/TyrfingMjolnir/dotfiles/master/.tidy.config
+
+cd /opt/local/dev/
+git clone https://github.com/tpope/vim-dadbod
+### git clone https://aur.archlinux.org/packages/swift-bin/
+git clone https://github.com/apple/swift
+git clone https://github.com/apple/sourcekit-lsp
+git clone https://github.com/apple/swift
+https://github.com/ratatui-org/ratatui
+
